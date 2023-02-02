b79bdc514b7f778736c2124d83a2721a134c9b38
ehimichi <m.pascone@campus.unimib.it>
2023-02-02 15:45:58 +0100

    Aggiornamento jsonparsev2.pl
    Tolta la parte di codice duplicato e aggiunta descrizione di ogni predicato

--- Prolog/jsonparsev2.pl
+++ Prolog/jsonparsev2.pl
@@ -1,405 +1,81 @@
 /*
- * inizio predicato jsonparse
+
+jsonaccess prende in input un jsonobject o un jsonarray e seguendo il
+contenuto di Field (lista, stringa o numero) restituisce un risultato.
+
 */
-
-jsonparse(JSONString, Object) :-
-    atom(JSONString),
-    var(Object),
-    !,
-    atom_codes(JSONString, Code_list),
-    riconosci_e_traduci(Code_list, Object).
-
-jsonparse(JSONString, Object) :-
-    string(JSONString),
-    var(Object),
-    !,
-    string_to_atom(JSONString, Atom),
-    jsonparse(Atom, Object).
-
-jsonparse(JSONString, Object) :-
-    nonvar(JSONString),
-    nonvar(Object),
-    !,
-    jsonparse(JSONString, Object1),
-    Object1 = Object.
-
-/*
- * fine predicato jsonparse
-*/
-
-/*
- * inizio predicato riconosci_e_traduci
- * che va a simulare il predicato value
- * ma solo su oggetti e array
-*/
-
-riconosci_e_traduci(Code_list, Result) :-
-    whitespace(Code_list, Codes_w_left),
-    riconosci_e_traduci_execute(Codes_w_left, Result).
-
-riconosci_e_traduci_execute([C | Cs], Result) :-
-    C = 123,
-    !,
-    object([C | Cs], Result, Codes_o_left),
-    whitespace(Codes_o_left, []).
-
-riconosci_e_traduci_execute([C | Cs], Result) :-
-    C = 91,
-    !,
-    array([C | Cs], Result, Codes_a_left),
-    whitespace(Codes_a_left, []).
-
-/*
- * fine predicato riconosci_e_traduci
-*/
-
-/*
- * inizio predicato value per riconoscimento
- * valori json
-*/
-
-value(Codes_in, Result, Codes_left) :-
-    whitespace(Codes_in, Codes_w_left),
-    value_execute(Codes_w_left, Result, Codes_left).
-
-value_execute([C | Cs], Result, Codes_left) :-
-    C = 123,
-    !,
-    object([C | Cs], Result, Codes_o_left),
-    whitespace(Codes_o_left, Codes_left).
-
-value_execute([C | Cs], Result, Codes_left) :-
-    C = 91,
-    !,
-    array([C | Cs], Result, Codes_a_left),
-    whitespace(Codes_a_left, Codes_left).
-
-value_execute([C | Cs], Result, Codes_left) :-
-    C = 116,
-    !,
-    true([C | Cs], Result, Codes_t_left),
-    whitespace(Codes_t_left, Codes_left).
-
-value_execute([C | Cs], Result, Codes_left) :-
-    C = 102,
-    !,
-    false([C | Cs], Result, Codes_f_left),
-    whitespace(Codes_f_left, Codes_left).
-
-value_execute([C | Cs], Result, Codes_left) :-
-    C = 110,
-    !,
-    null([C | Cs], Result, Codes_n_left),
-    whitespace(Codes_n_left, Codes_left).
-
-value_execute([C | Cs], Result, Codes_left) :-
-    C = 34,
-    !,
-    stringa([C | Cs], Result, Codes_s_left),
-    whitespace(Codes_s_left, Codes_left).
-
-value_execute(Codes_in, Result, Codes_left) :-
-    numero(Codes_in, Result, Codes_nu_left),
-    whitespace(Codes_nu_left, Codes_left).
-
-/*
- * fine predicato value
-*/
-
-/*
- * inizio predicato object per
- * riconoscimento oggetti json
-*/
-
-object(Codes_in, Result, Codes_left) :-
-    object_execute(Codes_in, Result, [], '', o0, Codes_left).
-
-object_execute([C | Cs], Result, Members, Temp, Q, Codes_left) :-
-    Q = o0,
-    C = 123,
-    !,
-    whitespace(Cs, Codes_w_left),
-    object_execute(Codes_w_left, Result, Members, Temp, o1, Codes_left).
-
-object_execute([C | Cs], Result, Members, _Temp, Q, Codes_left) :-
-    Q = o1,
-    C = 125,
-    !,
-    Result =.. [jsonobj, Members],
-    Codes_left = Cs.
-
-object_execute([C | Cs], Result, Members, Temp, Q, Codes_left) :-
-    Q = o1,
-    C \= 125,
-    !,
-    stringa([C | Cs], Result_stringa, Codes_s_left),
-    term_to_atom(Result_stringa, Atom),
-    atom_concat(Temp, Atom, Temp1),
-    whitespace(Codes_s_left, Codes_w_left),
-    object_execute(Codes_w_left, Result, Members, Temp1, o2, Codes_left).
-
-object_execute([C | Cs], Result, Members, Temp, Q, Codes_left) :-
-    Q = o2,
-    C = 58,
-    !,
-    atom_concat(Temp, ',', Temp1),
-    value(Cs, Result_value, Codes_v_left),
-    term_to_atom(Result_value, Atom),
-    atom_concat(Temp1, Atom, Temp2),
-    atom_to_term(Temp2, Term, _),
-    append(Members, [Term], Members1),
-    object_execute(Codes_v_left, Result, Members1, '', o3, Codes_left).
-
-object_execute([C | Cs], Result, Members, _Temp, Q, Codes_left) :-
-    Q = o3,
-    C = 125,
-    !,
-    Result =.. [jsonobj, Members],
-    Codes_left = Cs.
-
-object_execute([C | Cs], Result, Members, Temp, Q, Codes_left) :-
-    Q = o3,
-    C = 44,
-    !,
-    whitespace(Cs, Codes_w_left),
-    stringa(Codes_w_left, Result_stringa, Codes_s_left),
-    term_to_atom(Result_stringa, Atom),
-    atom_concat(Temp, Atom, Temp1),
-    whitespace(Codes_s_left, Codes_w1_left),
-    object_execute(Codes_w1_left, Result, Members, Temp1, o2, Codes_left).
-
-/*
- * fine predicato object
-*/
-
-/*
- * inizio predicato array per
- * riconoscimento array json
-*/
-
-array(Codes_in, Result, Codes_left) :-
-    array_execute(Codes_in, Result, [], a0, Codes_left).
-
-array_execute([C | Cs], Result, Elements, Q, Codes_left) :-
-    Q = a0,
-    C = 91,
-    !,
-    whitespace(Cs, Codes_w_left),
-    array_execute(Codes_w_left, Result, Elements, a1, Codes_left).
-
-array_execute([C | Cs], Result, Elements, Q, Codes_left) :-
-    Q = a1,
-    C = 93,
-    !,
-    Result =.. [jsonarray, Elements],
-    Codes_left = Cs.
-
-array_execute([C | Cs], Result, Elements, Q, Codes_left) :-
-    Q = a1,
-    C \= 93,
-    !,
-    value([C | Cs], Result_value, Codes_v_left),
-    append(Elements, [Result_value], Elements1),
-    array_execute(Codes_v_left, Result, Elements1, a2, Codes_left).
-
-array_execute([C | Cs], Result, Elements, Q, Codes_left) :-
-    Q = a2,
-    C = 93,
-    !,
-    Result =.. [jsonarray, Elements],
-    Codes_left = Cs.
-
-array_execute([C | Cs], Result, Elements, Q, Codes_left) :-
-    Q = a2,
-    C = 44,
-    !,
-    value(Cs, Result_value, Codes_v_left),
-    append(Elements, [Result_value], Elements1),
-    array_execute(Codes_v_left, Result, Elements1, a2, Codes_left).
-
-/*
- * fine predicato array
-*/
-
-/*
- * inizio predicato whitespace per rimozione
- * caratteri di spaziatura da una lista
- * di codici di caratteri in input
-*/
-
-whitespace([], []).
-
-whitespace([C | Cs], Codes_left) :-
-    whitespace_acceptable(C),
-    !,
-    whitespace(Cs, Codes_left).
-
-whitespace([C | Cs], Codes_left) :-
-    not(whitespace_acceptable(C)),
-    !,
-    Codes_left = [C | Cs].
-
-whitespace_acceptable(32). %spazio
-whitespace_acceptable(10). %\n
-whitespace_acceptable(13). %\r
-whitespace_acceptable(9).  %\t
-
-/*
- * fine predicato whitespace
-*/
-
-/*
- * inizio predicati per riconoscimento valori
- * elementari true, false e null
-*/
-
-true([A, B, C, D | Cs], Result, Codes_left) :-
-    atom_codes('true', [A, B, C, D]),
-    Result = true,
-    Codes_left = Cs.
-
-false([A, B, C, D, E | Cs], Result, Codes_left) :-
-    atom_codes('false', [A, B, C, D, E]),
-    Result = false,
-    Codes_left = Cs.
-
-null([A, B, C, D | Cs], Result, Codes_left) :-
-    atom_codes('null', [A, B, C, D]),
-    Result = null,
-    Codes_left = Cs.
-
-/*
- * fine predicati per riconoscimento valori elementari
-*/
-
-/*
- * inizio predicato per riconoscimento numeri
-*/
-
-numero(Codes_in, Result, Codes_left) :-
-    numero_execute(Codes_in, Result, '', Codes_left).
-
-numero_execute([], Result, Temp, Codes_left) :-
-    atom_to_term(Temp, Term, _),
-    number(Term),
-    Result = Term,
-    Codes_left = [].
-
-numero_execute([C | Cs], Result, Temp, Codes_left) :-
-    number_acceptable(C),
-    !,
-    atom_codes(Atom, [C]),
-    atom_concat(Temp, Atom, Temp1),
-    numero_execute(Cs, Result, Temp1, Codes_left).
-
-numero_execute([C | Cs], Result, Temp, Codes_left) :-
-    not(number_acceptable(C)),
-    !,
-    atom_to_term(Temp, Term, _),
-    number(Term),
-    Result = Term,
-    Codes_left = [C | Cs].
-
-number_acceptable(43).  %+
-number_acceptable(45).  %-
-number_acceptable(46).  %.
-number_acceptable(69).  %E
-number_acceptable(101). %e
-number_acceptable(Code) :-
-    Code >= 48,
-    Code =< 57.
-
-/*
- * fine predicato per riconoscimento numeri
-*/
-
-/*
- * inizio predicato per riconoscimento stringhe
-*/
-
-stringa(Codes_in, Result, Codes_left) :-
-    stringa_execute(Codes_in, Result, '', s0, Codes_left).
-
-stringa_execute([C | Cs], Result, Temp, Q, Codes_left) :-
-    Q = s0,
-    C = 34,
-    !,
-    atom_concat(Temp, '"', Temp1),
-    stringa_execute(Cs, Result, Temp1, s1, Codes_left).
-
-stringa_execute([C | Cs], Result, Temp, Q, Codes_left) :-
-    Q = s1,
-    C \= 34,
-    !,
-    atom_codes(Atom, [C]),
-    atom_concat(Temp, Atom, Temp1),
-    stringa_execute(Cs, Result, Temp1, s1, Codes_left).
-
-stringa_execute([C | Cs], Result, Temp, Q, Codes_left) :-
-    Q = s1,
-    C = 34,
-    !,
-    atom_concat(Temp, '"', Temp1),
-    atom_to_term(Temp1, Term, _),
-    string(Term),
-    Result = Term,
-    Codes_left = Cs.
-
-/*
- * fine predicato per riconoscimento stringhe
-*/
-
-/*
- * inizio predicati utils
-*/
-
-chiamabile(jsonobj).
-chiamabile(jsonarray).
-
-valore_base(true).
-valore_base(false).
-valore_base(null).
-
-
-/*
- * fine predicati utils
-*/
-
-
 
 
 %JSONACCESS CON ARRAY
+% Se in input ho un jsonarray, Field può essere solo un numero che
+% indica l'indice dell'array dal quale estrarre il valore.
+% Nel caso iterativo, viene chiamato elemento_i_esimo per cercare dentro
+% l'array.
+
+
 jsonaccess(jsonarray(_), [], jsonarray(_)).
 jsonaccess(jsonarray(Elements), Field, Risultato) :- elemento_i_esimo(Elements, Field, Risultato).
 
 
+/*
+jsonobject contiene una lista di coppie.
+In questo caso Field è una stringa o una lista.
+*/
+
 %JSONACCESS CON OBJECT - FIELD E' UNA LISTA
+% Quando Field è una lista, può contenere stringhe e/o numeri.
+% Prendo il primo elemento della lista Field e lo metto a confronto con
+% la chiave della prima coppia dell'ogetto json.
+% Se si trova una coppia json che unifica con Field, il valore
+% ritornato:
+% 1 CASO: E' un valore atomico (numero, stringa, true/false/null).
+% Allora questi viene restituito come risultato finale.
+% 2 CASO: E' un valore composto (jsonobject, jsonarray). Applico a
+% quest'ultimo il predicato jsonaccess (ricorsione).
+% Quando la lista è vuota viene restituito il risultato (caso base).
+%VEDERE MEGLIO IL CASO BASE E CAMBIARE IL MODELLO ITERATIVO NEL CASO.
+
 %Caso base:
 jsonaccess(jsonobj(Members), [], jsonobj(Members)).
 
-%Caso iterativo:
+%Caso iterativo - 1 CASO:
 jsonaccess(jsonobj(Members), [Field | []], RisultatoFinale) :-
     estrai_valore(Members, Field, Risultato),
     valore_base(Risultato),
     !,
     RisultatoFinale = Risultato.
 
-%Caso iterativo:
+%Caso iterativo - 2 CASO:
 jsonaccess(jsonobj(Members), [Field | Fields], RisultatoFinale) :-
     estrai_valore(Members, Field, Risultato),
     composto(Risultato),
     !,
     jsonaccess(Risultato, Fields, RisultatoFinale).
-%Queste scritture possono essere abbreviate
+
 
 
 %JSONACCESS CON OBJECT - FIELD E' UNA STRINGA
+% Se Field è una stringa complessa (ossia contiene più elementi, ad
+% esempio: "nome, cognome, 0, ...") allora viene divisa da un
+% predicato suddividi_field in due stringhe: la prima contenente il
+% primo di questi elementi ("nome"), e la seconda contenente il resto
+% della stringa. La prima stringa viene poi passata al predicato
+% estrai_valore che cerca tra tutte le coppie del jsonobject quella che
+% unifica con tale stringa, restituendo un risultato.
+% Il valore ritornato:
+% 1 CASO: E' un valore atomico (numero, stringa, true/false/null).
+% Allora questi viene restituito come risultato finale.
+% 2 CASO: E' un valore composto (jsonobject, jsonarray). Applico a
+% quest'ultimo il predicato jsonaccess (ricorsione).
+% Quando la stringa è vuota viene restituito il risultato (caso base).
+
+
 %Caso base:
 jsonaccess(jsonobj(Members), "", jsonobj(Members)).
 jsonaccess(jsonobj(Members), " ", jsonobj(Members)).
 
 
-%Caso iterativo:
+%Caso iterativo - 1 CASO:
 %Field è una stringa che contiene una chiave sola
 jsonacces(jsonobj(Members), Field, Risultato) :-
     suddividi_field(Field, PrimoField, _),
@@ -407,7 +83,7 @@ jsonacces(jsonobj(Members), Field, Risultato) :-
     valore_base(ParaRisultato),
     Risultato = ParaRisultato.
 
-%Caso iterativo:
+%Caso iterativo - 2 CASO:
 %Field è una stringa contenente più chiavi.
 jsonacces(jsonobj(Members), Field, Risultato) :-
     suddividi_field(Field, PrimoField, AltriField),
@@ -425,26 +101,29 @@ jsonacces(jsonobj(Members), Field, Risultato) :-
 
 %PARTE PREDICATI UTILS
 
+% Composto restituisce true se il valore passato è un jsonobject o un
+% jsonarray.
 composto(jsonobj(_)).
 composto(jsonarray(_)).
 
-%Già presenti sopra
-/*
- * valore_base(true).
- * valore_base(null).
- * valore_base(false).
-*/
+% valore_base restituisce true se il valore passato è una stringa, un
+% numero o è un true/false/null.
+
+valore_base(true).
+valore_base(null).
+valore_base(false).
 valore_base(number(_)).
 valore_base(string(_)).
 
 valore_base(X) :- not(composto(X)).
 
-%jsonarray può essere l'elemento iniziale oppure può essere un valore.
-%Per trovare l'N-esimo elemento uso il predicato
-%elemento_i_esimo.
-elemento_i_esimo(jsonarray(List), Index, Result) :-
+% Questo predicato prende in input un jsonarray e restituisce un
+% risultato in base al valore dell'indice. Per trovare l'N-esimo
+% elemento si utilizza il predicato elemento_i_esimo_execute che
+% percorre ricorsivamente la lista.
+elemento_i_esimo(jsonarray(Elements), Index, Result) :-
     number(Index),
-    elemento_i_esimo_execute(List, Index, Result).
+    elemento_i_esimo_execute(Elements, Index, Result).
 
 elemento_i_esimo_execute([I | _], Index, Result) :-
     Index = 0,
@@ -458,22 +137,33 @@ elemento_i_esimo_execute([_ | Is], Index, Result) :-
     elemento_i_esimo_execute(Is, X, Result).
 
 
-estrai_valore([(Chiave, Valore) | _], [Field], [Risultato]) :-
-    string(Field),
-    string(Chiave),
-    Field = Chiave,
-    !,
-    Valore = Risultato.
 
-estrai_valore([(Chiave, _) | _], [Field], fail) :-
-    string(Field),
+% estrai_valore riceve in input una lista di coppie (Chiave,Valore)
+% appartententi ad un jsonobject, e restituisce come risultato il valore
+% della coppia.
+% Il valore restituito è il valore della coppia (Chiave, Valore) quando
+% Chiave = Field.
+%Quando questo non succede, si passa alla coppia successiva.
+estrai_valore([], _, _) :- fail.
+estrai_valore([(Chiave, Valore) | _], Chiave, Valore).
+
+estrai_valore([(Chiave, _) | AltreCoppie], Field, Valore) :-
     string(Chiave),
     Field \= Chiave,
-    !.
+    estrai_valore(AltreCoppie, Field, Valore).
 
-estrai_valore([(_, _) | _], [Field], fail) :-
-    number(Field), !.
 
+% suddividi_field separa la stringa ricevuta in input (caso in cui Field
+% è una stringa), in stringa pre-carattere e stringa post-carattere,
+% dove 'carattere'= 44, ossia il char che identifica la virgola ( ',' ).
+% Questo predicato viene usato solo nel caso in cui la stringa è
+% complessa, ossia formata da più elementi, ad esempio:
+% Field = "nome, cognome, 0".
+% Allora il predicato crea due stringhe:
+% 1 stringa: "nome"
+% 2 stringa: "cognome, 0".
+% Che verranno usate all'interno di jsonaccess per la ricerca di un
+% valore (Result).
 suddividi_field(Stringa, PrimoField, SecondoField) :-
     string_codes(Stringa, ListaCodici),
     split_codice(ListaCodici, 44, PrimoCodice, SecondoCodice),
@@ -482,6 +172,10 @@ suddividi_field(Stringa, PrimoField, SecondoField) :-
     !.
 
 
+% split_codice separa una lista di caratteri al char desiderato creando
+% due liste separate:
+% 1 lista di codici pre-carattere
+% 1 lista di codici post-carattere.
 split_codice([X | Xs], T, [X | L1], L2) :-
                X\=T, split_codice(Xs, T, L1, L2), !.
 
