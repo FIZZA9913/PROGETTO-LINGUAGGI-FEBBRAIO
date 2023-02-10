d50c7bf9989e1d5d26f4feea590e2bc4d6d73fe5
ehimichi <m.pascone@campus.unimib.it>
2023-02-10 12:34:26 +0100

    prove

--- Lisp/jsonparse.lisp
+++ Lisp/jsonparse.lisp
@@ -11,3 +11,29 @@
              (= (first charlist) 9)
              (= (first charlist) 32)) (parsews (rest charlist)))
         (t charlist)))
+
+
+(defun parsebool_true (stcharlist)
+  (cond (= (first stcharlist) 116)
+        (cond (= (second stcharlist) 114)
+              (cond (= (third stcharlist) 117)
+                    (cond (= (fourth stcharlist) 101) (cons 'true rest(charlist))))))
+  (t (error "TRUE non è stato riconosciuto correttamente"))))
+                    
+
+(defun parsebool_false (stcharlist)
+  (cond (= (first stcharlist) #\f)
+        (cond (= (second stcharlist) #\a)
+              (cond (= (third stcharlist) #\l)
+                    (cond (= (fourth stcharlist) #\s) 
+                          (cond (= (fifth stcharlist) #\e) (return 'false'))))))
+  (t (error "~S non è un valore valido" (first stcharlist))))
+
+
+
+(defun parsebool_null (stcharlist)
+  (cond (= (first stcharlist) #\n)
+        (cond (= (second stcharlist) #\u)
+              (cond (= (third stcharlist) #\l)
+                    (cond (= (fourth stcharlist) #\l) (return 'null')))))
+  (t (error "~S non è un valore valido" (first stcharlist))))
